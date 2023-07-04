0436923c5df28495f9b12b3cf9f448abc22f83c7
Javohir <javohir.hamdamov1@icloud.com>
2023-07-04 10:01:12 +0300

    number type prog

--- v1.py
+++ v1.py
@@ -1,1 +1,12 @@
 # javohir
+a = int(input("a="))
+if a == 0:
+    print("son nolga teng")
+elif a > 0 and a % 2 ==0:
+    print("musbat juft son")
+elif a > 0 and a % 2 == 1:
+    print("musbat toq son")
+elif a<0 and abs(a) % 2 == 0:
+    print("manfiy juft son")
+else:
+    print("manfiy toq son")
