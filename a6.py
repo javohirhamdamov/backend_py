8bb71a6145c3926b0f050751805d50dc92a28e1c
Javohir <javohir.hamdamov1@icloud.com>
2023-07-15 15:13:14 +0500

    list

---
+++ a6.py
@@ -0,0 +1,12 @@
+States = ["Uzbekistan","Turkiya","Canada","Korea","Shvesariya"]
+cons_states = States.copy()
+print(len(States))
+print(sorted(States))
+print(sorted(States,reverse=True))
+print(States)
+States.reverse()
+print(States)
+States.sort()
+print(States)
+States.sort(reverse=True)
+print(States)
