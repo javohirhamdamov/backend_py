9fff6630d294608af302cd0edb359264cb579590
Javohir <javohir.hamdamov1@icloud.com>
2023-07-15 13:25:33 +0500

    list_amaly

---
+++ a4.py
@@ -0,0 +1,4 @@
+t_shaxslar = ["Amir Temur", "Alisher Navoiy"]
+z_shaxslar = ["Bill Gates", "Elon Musk"]
+print("Men tarixiy shaxylardan " + t_shaxslar[0] + " hayotiga qiziqaman.")
+print("Zamonaviy shaxslardan " + z_shaxslar[1] + " bilan suhbatlashishni xoxlayman." )
