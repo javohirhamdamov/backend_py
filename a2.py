4fd23c9e0e7d17c7afea490ca9b124d6d7e50c62
Javohir <javohir.hamdamov1@icloud.com>
2023-07-16 20:55:12 +0500

    list

---
+++ a2.py
@@ -0,0 +1,12 @@
+taomlar = ["manti", "xonim", "somsa", "sendwich", "olivia salati"]
+nonushta = taomlar.copy()
+nonushta.pop(0)
+nonushta.pop(0)
+nonushta.pop(0)
+nonushta.append("murabbo")
+nonushta.append("qaymoq")
+print(taomlar)
+print(nonushta)
+nonushta = tuple(nonushta)
+# nonushta[0] = "qaymoq va non"
+print(nonushta)
